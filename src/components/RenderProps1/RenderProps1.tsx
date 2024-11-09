4064d370e305cd200ffb9220d14babe381548470
Cristian Contreras <crontreras1@gmail.com>
2024-11-09 15:14:06 -0500

    class 2. render props

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element
+}
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name:string = 'Crontreras1'
+
+  return (
+    <div>
+      { render(name) }
+    </div>
+  );
+};
+
 export const ParentComponent = () => {
   return (
-    <>
-    </>
+    < ChildComponent render={ name => <p>Hola, { name }!</p> } />
   );
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,17 @@
-const ParentComponent = () => {
+import React, { useState } from "react";
+
+interface ParentComponentProps {
+    render: (data: string[]) => React.ReactNode
+}
+
+const fruits: string[] = ['Apple 🍎', 'Graps 🍇', 'Orange 🍊']
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+    const [ data ] = useState<string[]>(fruits)
+
+    return(
+        <ul>{ render(data) }</ul>
+    )
 };
 
 export default ParentComponent;
