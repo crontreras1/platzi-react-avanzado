cf27a63c870aec41cb44f7137e07b1efa4050036
Cristian Contreras <crontreras1@gmail.com>
2024-11-09 16:58:25 -0500

    class 5. High Order Components

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,28 @@
+import React from "react";
+
+function withLoading<T extends object> (Component: React.ComponentType<T>) {
+  return (props: T & { isLoading: boolean } ) => {
+    const { isLoading, ...rest } = props 
+    return isLoading ? <p>Loading...</p> : < Component { ...rest as T } />
+  }
+}
+
+type UserComponentProps = {
+  name: string
+}
+
+const UserComponent: React.FC<UserComponentProps> = ({ name }) => {
+  return <p>Hola, { name }!</p>
+}
+
+const UserWithLoading = withLoading(UserComponent)
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={ true } name='Crontreras1' />
+  
+      <UserWithLoading isLoading={ false } name='Crontreras1'/>
     </div>
   );
 };
