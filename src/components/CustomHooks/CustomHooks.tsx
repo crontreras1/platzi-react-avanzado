12041e68457aa49e42e278108986426c10aea9d2
Cristian Contreras <crontreras1@gmail.com>
2024-11-09 17:18:53 -0500

    class 6. custom hook

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,34 @@
-const UserComponent = () => {
+import React, { useState } from "react";
+
+function useLoading (initialLoading: boolean = false) {
+  const [ isLoading, setIsLoading ] = useState(initialLoading)
+
+  return {
+    isLoading,
+    setIsLoading
+  }
+}
+
+type UserComponentProps = {
+  name: string
+}
+
+const UserComponent: React.FC<UserComponentProps> = ({ name }) => {
+  const { isLoading, setIsLoading } = useLoading()
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
+      <p>Hello, { name }! </p>
+
+      <button onClick={ () => setIsLoading(true) }>Simulate Loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name='Crontreras1' />;
 };
